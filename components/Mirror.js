e030a52f1e3de92b6f899e18160249dc9ed99223
Niccolò Segato <segato117@gmail.com>
2021-01-05 19:04:44 +0100

    New shut down button and removed logout from home

--- components/Mirror.js
+++ components/Mirror.js
@@ -6,7 +6,8 @@ import {
   View,
   Text,
   Image,
-  StatusBar,
+  Button,
+  TouchableOpacity,
 } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
@@ -17,6 +18,7 @@ const Mirror: () => React$Node = () => {
     const [name, setName] = useState();
     const [image, setImage] = useState();
     const [status, setStatus] = useState();
+    const [mirrorID, setMirror] = useState();
 
     // Set an initializing state whilst Firebase connects
     const [initializing, setInitializing] = useState(true);
@@ -36,13 +38,14 @@ const Mirror: () => React$Node = () => {
 
     firestore().collection('users').doc(user.uid).get().then((doc)=>{ 
         setName(doc.data().name);
+        setMirror(doc.data().mirrorID);
         firestore().collection('mirrors').doc(doc.data().mirrorID).get().then((document)=>{ 
-            setStatus(document.data().status)
             setImage(document.data().image)
         })
-        .catch(e => console.log(e));
+        firestore().collection('mirrors').doc(doc.data().mirrorID).onSnapshot(function(snap) {
+            setStatus(snap.data().status)
+        })
     })
-    .catch(e => console.log(e));
 
 
     return (
@@ -54,6 +57,8 @@ const Mirror: () => React$Node = () => {
                     <View style={styles.statusCircle} backgroundColor={'#008000'} />
                     ) : status === 'refresh' ? (
                         <View style={styles.statusCircle} backgroundColor={'#FFFF00'} />
+                    ) : status === 'sleep mode' ? (
+                        <View style={styles.statusCircle} backgroundColor={'#FFFF00'} />
                     ) : status === 'turned off' && (
                         <View style={styles.statusCircle} backgroundColor={'#FF0000'} />
                     )}
@@ -61,6 +66,31 @@ const Mirror: () => React$Node = () => {
             </View>
             <Image source={{uri: image}} 
             style={styles.mirrorPic}/>
+            <View>
+                <TouchableOpacity  style={styles.turOff}
+                onPress={() => status === 'running' ? (
+                    firestore().collection('mirrors').doc(mirrorID).set({ status: 'turned off'}, { merge: true })
+                    ) : status === 'sleep mode' ? (
+                        firestore().collection('mirrors').doc(mirrorID).set({ status: 'running'}, { merge: true })
+                    ) : status === 'turned off' && (
+                        firestore().collection('mirrors').doc(mirrorID).set({ status: 'running'}, { merge: true })
+                    )}
+                >
+                    <Image source={require('../assets/images/shutdown.png')}/>
+                </TouchableOpacity>
+                <Button
+                onPress={() => status === 'running' ? (
+                    firestore().collection('mirrors').doc(mirrorID).set({ status: 'sleep mode'}, { merge: true })
+                    ) : status === 'sleep mode' ? (
+                        firestore().collection('mirrors').doc(mirrorID).set({ status: 'running'}, { merge: true })
+                    ) : status === 'turned off' && (
+                        firestore().collection('mirrors').doc(mirrorID).set({ status: 'sleep mode'}, { merge: true })
+                    )}
+                title="SLEEP"
+                color="#841584"
+                accessibilityLabel="Turn Off"
+                />
+            </View>
         </View>
     )
 }
@@ -99,6 +129,12 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         marginTop: 12,
         marginRight: 5
+    },
+    turnOff: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        backgroundColor: '#DDDDDD'
     }
 });
 
